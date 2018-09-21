8a5624293f1bfdce61695505341c73b46f4234cf
Nicholas Gambino <nicholasgambino@Nicholass-MacBook-Pro.local>
2018-09-20 20:51:27 -1000

    connected api

--- index.js
+++ index.js
@@ -10,30 +10,29 @@ require('dotenv').config()
 
 
 
-let token = process.env.NOAA_API_KEY;
+let token = 'JsVvJzFOKezEWtnZPSZjdXyKabOwYdEj';
 
 
 
-app.get('/', (req,res) => {
-	let startdate = '2010-05-01';
-	let enddate = '2010-05-01';
-	let url = `https://www.ncdc.noaa.gov/cdo-web/api/v2/data?datasetid=GHCND&locationid=ZIP:28801&startdate=${startdate}&enddate=${enddate}
+app.get('/', (req, res) => {
+  let startdate = '2010-05-01';
+  let enddate = '2010-05-01';
+  let url = `https://www.ncdc.noaa.gov/cdo-web/api/v2/data?datasetid=GHCND&locationid=ZIP:28801&startdate=${startdate}&enddate=${enddate}
 `;
-	axios.get(url, {headers: { 
-			token: token
-		}
-	})
-	.then(response => {
-		res.json(response.data)
-	})
-	.catch(err => {
-		console.log(err);
-		res.json('oh nooooooooo! it brokesh*t')
-	})
+  axios.get(url, {
+    headers: {
+      token: token
+    }
+  })
+    .then(response => {
+      res.json(response.data)
+    })
+    .catch(err => {
+      console.log(err);
+      res.json('oh nooooooooo! it brokesh*t')
+    })
 });
 
 
-
-
-app.listen(9000);
+app.listen(2500);
 
