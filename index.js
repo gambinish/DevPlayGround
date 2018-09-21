1a30416c80b311cbbc8c7519bbc4bc469e3388f3
Nicholas Gambino <nicholasgambino@Nicholass-MacBook-Pro.local>
2018-09-20 22:26:58 -1000

    refactor2

--- index.js
+++ index.js
@@ -3,12 +3,12 @@ const app = express();
 
 const axios = require('axios');
 
-let url = '';
+let url = 'google.com';
 
 app.get('/', (req, res) => {
   axios.get(url)
     .then(response => {
-      console.log(response);
+      console.log(response.data);
       res.json('hello world');
     })
     .catch(err => {
